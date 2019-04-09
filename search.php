68fdfe72d8ff136470dc2ff02d1ac60f45e46f02
Gekif <dzulfikar.maulana@gmail.com>
2019-04-09 10:44:45 +0700

    02-Getting Started: Running Our Application

--- search.php
+++ search.php
@@ -17,4 +17,16 @@ if (!empty($search)) {
     if (!$search_query) {
         die('QUERY FAILED ' . mysqli_error($connection));
     }
+
+    while ($row = mysqli_fetch_array($search_query)) {
+        $brand = $row['cars'];
+        ?>
+        <ul class="list-unstyled">
+            <?php
+                echo "<li>{$brand} in stock</li>"
+            ?>
+        </ul>
+<?php
+    }
 }
+?>
