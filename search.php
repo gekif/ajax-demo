e6224f233df25553a8a6bb8b9e20469fbd3d8540
Gekif <dzulfikar.maulana@gmail.com>
2019-04-11 09:33:38 +0700

    05-Updating and Deleting Data: No Database Info Available Functionality and Clearing Text Field

--- search.php
+++ search.php
@@ -5,22 +5,29 @@ $search = $_POST['search'];
 //echo $search;
 
 if (!empty($search)) {
-    $query = "SELECT * FROM cars WHERE title LIKE '%$search%'";
+    $query = "SELECT * FROM cars WHERE title LIKE '$search%'";
     $search_query = mysqli_query($connection, $query);
+    $count = mysqli_num_rows($search_query);
 
     if (!$search_query) {
         die('QUERY FAILED ' . mysqli_error($connection));
     }
 
-    while ($row = mysqli_fetch_array($search_query)) {
-        $brand = $row['title'];
-        ?>
-        <ul class="list-unstyled">
-            <?php
-                echo "<li>{$brand} in stock</li>"
+    if ($count <= 0) {
+        echo "Sorry we don't have that car available";
+    } else {
+        while ($row = mysqli_fetch_array($search_query)) {
+            $brand = $row['title'];
             ?>
-        </ul>
-<?php
+            <ul class="list-unstyled">
+                <?php
+                echo "<li>{$brand} in stock</li>"
+                ?>
+            </ul>
+            <?php
+        }
     }
+
+
 }
 ?>
