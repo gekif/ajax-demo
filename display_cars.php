5377f4eaa3324ad17d4d7e642ff6a6a16ff4664e
Gekif <dzulfikar.maulana@gmail.com>
2019-04-11 10:07:09 +0700

    05-Updating and Deleting Data: New PHP File and Data Extraction

--- display_cars.php
+++ display_cars.php
@@ -17,18 +17,32 @@ while ($row = mysqli_fetch_array($query_car_info)) {
     echo "<tr>";
 
     echo "<td>" . $i++ . "</td>";
-    echo "<td><a class='title-link' href='#'>{$row['title']} </a></td>";
-
+//    echo "<td><a class='title-link' href='#'>{$row['title']} </a></td>";
+//    echo "<td><a rel='".$row['id']."' class='title-link' href='#'>{$row['title']} </a></td>";
+    echo "<td><a rel='".$row['id']."' class='title-link' href='javascript:void(0)'>{$row['title']}</a></td>";
     echo "</tr>";
 
 }
 ?>
 <script>
     // Action Container
-    $('#action-container').hide();
+//    $('#action-container').hide();
 
     $('.title-link').on('click', function () {
         $('#action-container').show();
+
+        var id = $(this).attr('rel');
+
+//        alert(id);
+
+        $.post('process.php', { id: id}, function(data) {
+//            alert(data);
+
+            $('#action-container').html(data);
+
+
+        });
+
     });
 
 </script>
---
+++ process.php
@@ -0,0 +1,24 @@
+<?php
+
+include('db.php');
+
+echo 'This is shit';
+
+/*$query = "SELECT * FROM cars";
+$query_car_info = mysqli_query($connection, $query);
+
+
+if (!$query_car_info) {
+    die("Query Failed " . mysqli_error($connection));
+}
+
+$i = 1; // Init index of number
+while ($row = mysqli_fetch_array($query_car_info)) {
+
+    echo "<tr>";
+
+    echo "<td>" . $i++ . "</td>";
+    echo "<td><a class='title-link' href='#'>{$row['title']} </a></td>";
+    echo "</tr>";
+
+}*/
