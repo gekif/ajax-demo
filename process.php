fefd347f0a39b1a549795be722e7cc327c84d7c1
Gekif <dzulfikar.maulana@gmail.com>
2019-04-11 10:53:49 +0700

    05-Updating and Deleting Data: Update functionality part 2: Button Click

--- process.php
+++ process.php
@@ -4,36 +4,51 @@ include('db.php');
 
 //echo 'This is shit';
 
+
+/**
+ * Displaying Action Box Data
+*/
+
 if (isset($_POST['id'])) {
+
     $id = mysqli_real_escape_string($connection, $_POST['id']);
+
+    $query = "SELECT * FROM cars WHERE id =  {$id}" ;
+    $query_car_info = mysqli_query($connection, $query);
+
+    if (!$query_car_info) {
+        die("Query Failed " . mysqli_error($connection));
+    }
+
+    while ($row = mysqli_fetch_array($query_car_info)) {
+        echo "<input rel='".$row['id']."' type='text' class='form-control title-input' value='".$row['title']."'>";
+        echo "<input type='button' class='btn btn-success update' value='Update' >";
+        echo "<input type='button' class='btn btn-danger delete' value='Delete'>";
+
+    }
 }
 
-$query = "SELECT * FROM cars WHERE id =  {$id}" ;
-$query_car_info = mysqli_query($connection, $query);
 
+/**
+ * Updating Data
+*/
 
-if (!$query_car_info) {
-    die("Query Failed " . mysqli_error($connection));
-}
-
-
-while ($row = mysqli_fetch_array($query_car_info)) {
-
-    echo "<input rel='".$row['id']."' type='text' class='form-control title-input' value='".$row['title']."'>";
-    echo "<input type='button' class='btn btn-success car_id' value='Update' >";
-    echo "<input type='button' class='btn btn-danger car_id' value='Delete'>";
-
+if (isset($_POST['updatethis'])) {
+    echo 'it works';
 }
 
 ?>
+
 
 <script>
 
     $(document).ready(function() {
 
-        // Global Variable for id and title
+        // Global Variable
         var id;
         var title;
+        var updatethis = 'update';
+        var deletethis = 'delete';
 
         /**
          * Extract id and title
@@ -45,12 +60,23 @@ while ($row = mysqli_fetch_array($query_car_info)) {
 
 //            alert(title);
 
-            /**
-             * Update Button Function
-             */
-
-
         });
+
+        /**
+         * Update Button Function
+         */
+        $('.update').on('click', function() {
+//            alert('tai');
+
+            $.post('process.php', {
+                id: id,
+                title: title,
+                updatethis: updatethis,
+                deletethis: deletethis
+            }, function (data) {
+                alert(data);
+            });
+        })
 
 
 
